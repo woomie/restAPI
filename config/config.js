a53d263c61324b535f0e54b45270ad2a675b81a7
Wunmi <wunmilawal24@gmail.com>
2026-02-17 14:56:30 -0700

    Move Firebase credentials to .env for security

--- config/config.js
+++ config/config.js
@@ -1,5 +1,16 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./restapi-1d162-firebase-adminsdk-fbsvc-dfd812bfb5.json");
+//const serviceAccount = require("./restapi-1d162-firebase-adminsdk-fbsvc-dfd812bfb5.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+};
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
